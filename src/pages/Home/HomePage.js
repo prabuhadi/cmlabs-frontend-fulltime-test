52842125722de793b8b084a29bced6be65f9711a
prabuhadi <prabuhadiyo@gmail.com>
2023-03-04 00:06:13 +0700

    Modify meal detail page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -3,10 +3,22 @@ import "./HomePage.scss";
 import { useMealContext } from "../../context/mealContext";
 import Loader from "../../components/Loader/Loader";
 import CategoryList from "../../components/Category/CategoryList";
+import NotFound from "../../components/NotFound/NotFound";
+import MealList from "../../components/Meal/MealList";
 
 const HomePage = () => {
+  const { categories, meals, categoryLoading, mealsLoading } = useMealContext();
   return (
     <main className="main-content">
+      {mealsLoading ? (
+        <Loader />
+      ) : meals === null ? (
+        <NotFound />
+      ) : meals?.length ? (
+        <MealList meals={meals} />
+      ) : (
+        ""
+      )}
       {categoryLoading ? <Loader /> : <CategoryList categories={categories} />}
     </main>
   );
