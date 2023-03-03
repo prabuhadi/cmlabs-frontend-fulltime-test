87c37e91f7f3d39b701598b4e8a804b957ad54b5
prabuhadi <prabuhadiyo@gmail.com>
2023-03-03 19:37:51 +0700

    modify navbar content

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -6,8 +6,28 @@ import { IoMdMenu } from "react-icons/io";
 import { useSidebarContext } from "../../context/sidebarContext";
 
 const Navbar = () => {
+  const { openSidebar } = useSidebarContext();
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 60) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
   return (
-    <nav className={"navbar bg-orange flex align-center"}>
+    <nav
+      className={`navbar bg-orange flex align-center ${
+        scrolled ? "scrolled" : ""
+      }`}
+    >
       <div className="container w-100">
         <div className="navbar-content text-white">
           <div className="brand-and-toggler flex align-center justify-between">
@@ -16,7 +36,11 @@ const Navbar = () => {
               <span className="navbar-brand-text fw-7">FoodPedia</span>
             </Link>
             <div className="navbar-btns flex align-center">
-              <button type="button" className="navbar-show-btn text-white">
+              <button
+                type="button"
+                className="navbar-show-btn text-white"
+                onClick={() => openSidebar()}
+              >
                 <IoMdMenu size={27} />
               </button>
             </div>
