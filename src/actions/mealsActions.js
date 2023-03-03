72fd8e8d8d5dbeaf714056a158fd514084db6f7a
prabuhadi <prabuhadiyo@gmail.com>
2023-03-03 21:33:13 +0700

    fix category list

--- src/actions/mealsActions.js
+++ src/actions/mealsActions.js
@@ -3,6 +3,12 @@ import {
   FETCH_CATEGORY_BEGIN,
   FETCH_CATEGORY_ERROR,
   FETCH_CATEGORY_SUCCESS,
+  FETCH_CATEGORY_MEALS_BEGIN,
+  FETCH_CATEGORY_MEALS_ERROR,
+  FETCH_CATEGORY_MEALS_SUCCESS,
+  FETCH_SINGLE_MEAL_BEGIN,
+  FETCH_SINGLE_MEAL_ERROR,
+  FETCH_SINGLE_MEAL_SUCCESS,
 } from "./actions";
 
 import {
@@ -22,5 +28,28 @@ export const startFetchCategories = async (dispatch) => {
     });
   } catch (error) {
     dispatch({ type: FETCH_CATEGORY_ERROR, payload: error.message });
+  }
+};
+
+export const startFetchSingleMeal = async (dispatch, id) => {
+  try {
+    dispatch({ type: FETCH_SINGLE_MEAL_BEGIN });
+    const response = await axios.get(`${MEAL_SINGLE_URL}${id}`);
+    dispatch({ type: FETCH_SINGLE_MEAL_SUCCESS, payload: response.data.meals });
+  } catch (error) {
+    dispatch({ type: FETCH_SINGLE_MEAL_ERROR, payload: error.message });
+  }
+};
+
+export const startFetchMealByCategory = async (dispatch, category) => {
+  try {
+    dispatch({ type: FETCH_CATEGORY_MEALS_BEGIN });
+    const response = await axios.get(`${MEAL_CATEGORIES_URL}${category}`);
+    dispatch({
+      type: FETCH_CATEGORY_MEALS_SUCCESS,
+      payload: response.data.meals,
+    });
+  } catch (error) {
+    dispatch({ type: FETCH_CATEGORY_MEALS_ERROR, payload: error.message });
   }
 };
