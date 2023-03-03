fa6ed53f325ebf35b7713c3efa125b5babc75bee
prabuhadi <prabuhadiyo@gmail.com>
2023-03-03 17:36:38 +0700

    adding pages

---
+++ src/pages/MealDetails/MealDetailsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MealDetailsPage = () => {
+  return <div>MealDetailsPage</div>;
+};
+
+export default MealDetailsPage;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "../pages/Home/HomePage";
+import Error from "../pages/Error/ErrorPage";
+import MealDetails from "../pages/MealDetails/MealDetailsPage";
+import Category from "../pages/Category/CategoryPage";
+
+export { Home, Error, MealDetails, Category };
